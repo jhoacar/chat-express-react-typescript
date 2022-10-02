1db3f08d9a545f69e9b4d048087f4f9c43a76030
jhoacar <carrerojhoan@gmail.com>
2022-10-02 20:22:56 -0300

    added peer dependecy in server

--- server/src/boot/server.ts
+++ server/src/boot/server.ts
@@ -1,11 +1,29 @@
 import express from 'express';
 // import morgan from 'morgan';
 import router from '@routes';
+import { createServer } from 'http';
+import { Server, Socket } from 'socket.io';
 
 const app = express();
 
 // app.use(morgan('combined'));
-
 app.use(router);
 
-export default app;
+const server = createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+  },
+});
+
+io.on('connecion', (socket: Socket) => {
+  const { address, issued } = socket.handshake;
+
+  console.log(`New connection from ${address} - ${new Date(issued)}`);
+
+  socket.on('join', (data) => {
+    console.log('User connected send: ', data);
+  });
+});
+
+export default server;
