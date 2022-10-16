55a5e45dd5dba236968919dba3c61d41e64b9726
jhoacar <carrerojhoan@gmail.com>
2022-10-16 01:55:35 -0300

    added websocketID

--- client/src/redux/states/connection.ts
+++ client/src/redux/states/connection.ts
@@ -37,6 +37,7 @@ export const connectionSlice = createSlice({
     },
     disconnectWebSocket: (state) => {
       removeWebSocketID();
+      removePeerID();
       state.socketID = '';
       return state;
     },
